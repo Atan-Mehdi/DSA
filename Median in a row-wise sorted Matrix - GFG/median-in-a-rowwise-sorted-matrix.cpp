ea2f41860c3ce9ab6760529b95a7797cec2e3334
Atan Mehdi <90546797+Atan-Mehdi@users.noreply.github.com>
2023-09-27 18:04:10 +0530

    Added solution - LeetHub

---
+++ Median in a row-wise sorted Matrix - GFG/median-in-a-rowwise-sorted-matrix.cpp
@@ -0,0 +1,74 @@
+//{ Driver Code Starts
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+//User function template for C++
+
+class Solution{   
+public:
+int countSmallerThanMid(vector<int> &row, int mid)
+{
+  int l = 0, h = row.size() - 1;
+  while (l <= h)
+  {
+    int md = (l + h) >> 1;
+    if (row[md] <= mid)
+    {
+      l = md + 1;
+    }
+    else
+    {
+      h = md - 1;
+    }
+  }
+  return l;
+}
+    int median(vector<vector<int>> &A, int R, int C){
+      int low = 1;
+  int high = 1e9;
+  int n = A.size();
+  int m = A[0].size();
+  while (low <= high)
+  {
+    int mid = (low + high) >> 1;
+    int cnt = 0;
+    for (int i = 0; i < n; i++)
+    {
+      cnt += countSmallerThanMid(A[i], mid);
+    }
+    if (cnt <= (n * m) / 2)
+      low = mid + 1;
+    else
+      high = mid - 1;
+  }
+  return low;        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int r, c;
+        cin>>r>>c;
+        vector<vector<int>> matrix(r, vector<int>(c));
+        for(int i = 0; i < r; ++i)
+            for(int j = 0;j < c; ++j)
+                cin>>matrix[i][j];
+        Solution obj;
+        int ans=-1;
+        ans=obj.median(matrix, r, c);
+        cout<<ans<<"\n";        
+    }
+    return 0;
+}
+// } Driver Code Ends
